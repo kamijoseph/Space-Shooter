255bfe9ceee6b06d57031b9c0adec922f028e260
Kami <josephkiarie561@gmail.com>
2025-10-30 22:02:17 +0300

    star surfaces

--- code/main.py
+++ code/main.py
@@ -2,6 +2,7 @@
 # pygame space shooter game
 import pygame
 from os.path import join
+from random import randint
 
 # initialise
 pygame.init()
@@ -11,16 +12,18 @@ WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
 display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption("Space Shooter Game")
-running = True
 
-# plain surface
-surf = pygame.Surface((100, 200))
-surf.fill("orange")
-x = 100
-
-# image surfaces
+# player surfaces
 player_surf = pygame.image.load(join("images", "player.png")).convert_alpha()
+x, y = 100, 150
 
+# stars surface
+star_surf = pygame.image.load(join("images", "star.png")).convert_alpha()
+star_positions = [(randint(0, WINDOW_WIDTH), randint(0, WINDOW_HEIGHT)) for _ in range(20)]
+
+
+
+running = True
 while running:
     # event loop
     for event in pygame.event.get():
@@ -28,9 +31,20 @@ while running:
             running = False
 
     # draw the game
-    display_surface.fill("darkgray")
+    display_surface.fill("black")
+
+    # displaying the stars surface
+    for position in star_positions:
+        display_surface.blit(
+            star_surf,
+            position
+        )
+
+    # displaying the player surface
     x += 0.5
-    display_surface.blit(player_surf, (x, 150))
+    display_surface.blit(player_surf, (x, y))
+
+    
     pygame.display.update()
     
 
