88d08663dba2f39c04f0bd56c4c5bcffbfd63d6b
Kami <josephkiarie561@gmail.com>
2025-10-30 19:52:35 +0300

    basic project initialization

--- code/main.py
+++ code/main.py
@@ -1,2 +1,21 @@
 
 # pygame space shooter game
+import pygame
+
+# initialise
+pygame.init()
+
+# window/display_surface
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 720
+display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+running = True
+
+while running:
+    # event loop
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # draw the game
+pygame.quit()
