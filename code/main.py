7b342b4dcfe34e9600a8df88b9079e8e292a55cd
Kami <josephkiarie561@gmail.com>
2025-11-15 15:03:40 +0300

    updating sprite s with oop

--- code/main.py
+++ code/main.py
@@ -4,8 +4,18 @@ import pygame
 from os.path import join
 from random import randint
 
+class Player(pygame.sprite.Sprite):
+    def __init__(self, groups):
+        super().__init__(groups)
+        self.image = pygame.image.load(join("images", "player.png")).convert_alpha()
+        self.rect = self.image.get_frect(center = (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
+    
+    def update(self):
+        print("ship is being updated........")
+
 # initialise
 pygame.init()
+
 
 # window/display_surface
 WINDOW_WIDTH = 1280
@@ -14,13 +24,17 @@ display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption("Space Shooter Game")
 clock = pygame.time.Clock()
 FPS = 120
+x, y = 100, 150
+
+
+all_sprites = pygame.sprite.Group()
+player = Player(all_sprites)
 
 # player surfaces
-x, y = 100, 150
-player_surf = pygame.image.load(join("images", "player.png")).convert_alpha()
-player_rect = player_surf.get_frect(center = (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
-player_direction = pygame.math.Vector2(0, 0)
-player_speed = 300
+# player_surf = pygame.image.load(join("images", "player.png")).convert_alpha()
+# player_rect = player_surf.get_frect(center = (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
+# player_direction = pygame.math.Vector2(0, 0)
+# player_speed = 300
 
 
 # stars surface
@@ -48,16 +62,18 @@ while running:
             running = False
         
     # user inputs
-    keys = pygame.key.get_pressed()
-    # moving left and righht
-    player_direction.x = int(keys[pygame.K_RIGHT]) - int(keys[pygame.K_LEFT])
-    # moving up and down
-    player_direction.y = int(keys[pygame.K_DOWN]) - int(keys[pygame.K_UP])
-    # handling diagonal inconsistent speed
-    player_direction = player_direction.normalize() if player_direction else player_direction
+    # keys = pygame.key.get_pressed()
+    # # moving left and righht
+    # player_direction.x = int(keys[pygame.K_RIGHT]) - int(keys[pygame.K_LEFT])
+    # # moving up and down
+    # player_direction.y = int(keys[pygame.K_DOWN]) - int(keys[pygame.K_UP])
+    # # handling diagonal inconsistent speed
+    # player_direction = player_direction.normalize() if player_direction else player_direction
 
-    # moving player with input
-    player_rect.center += player_direction * player_speed * dt
+    # # moving player with input
+    # player_rect.center += player_direction * player_speed * dt
+
+    all_sprites.update()
 
     # draw the game
     display_surface.fill("black")
@@ -72,7 +88,8 @@ while running:
     # displaying ship, meteor and laser
     display_surface.blit(meteor_surf, meteor_rect)
     display_surface.blit(laser_surf, laser_rect)
-    display_surface.blit(player_surf, player_rect)
+    # display_surface.blit(player_surf, player_rect)
+    all_sprites.draw(display_surface)
 
     pygame.display.update()
     
