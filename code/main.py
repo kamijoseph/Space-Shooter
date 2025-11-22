8c4c0dded1dc02b2297832292173675a0b1b196f
Kami <josephkiarie561@gmail.com>
2025-11-22 20:06:01 +0300

    display score function

--- code/main.py
+++ code/main.py
@@ -91,6 +91,13 @@ def collisions():
         if collided_sprites:
             laser.kill()
 
+# display score
+def display_score():
+    current_time = pygame.time.get_ticks()
+    text_surf = font.render(str(current_time), True, (240, 240, 240))
+    text_rect = text_surf.get_frect(midbottom = (WINDOW_WIDTH / 2, WINDOW_HEIGHT - 50))
+    display_surface.blit(text_surf, text_rect)
+
 # initialise
 pygame.init()
 
@@ -107,7 +114,6 @@ star_surf = pygame.image.load(join("images", "star.png")).convert_alpha()
 meteor_surf = pygame.image.load(join("images", "meteor.png")).convert_alpha()
 laser_surf = pygame.image.load(join("images", "laser.png")).convert_alpha()
 font = pygame.font.Font(join("images", "Oxanium-Bold.ttf"), 20)
-text_surf = font.render("text", True, (240, 240, 240))
 
 # sprites
 all_sprites = pygame.sprite.Group()
@@ -139,12 +145,10 @@ while running:
     collisions()
 
 
-    # drawing the game
+    # idsplays and drawings
     display_surface.fill("#3a2e3f")
     all_sprites.draw(display_surface)
-    
-    # text
-    display_surface.blit(text_surf,(0, 0))
+    display_score()
 
 
     pygame.display.update()
